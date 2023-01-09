9b9552f92fc86d1ddd8abab1885513b89ac7f423
rasedprogrammer <rasedprogrammer@gmail.com>
2023-01-09 19:45:15 +0600

    adding doctors portal server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// Middleware configuration
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+	res.send("Doctors Portal Server Is Running");
+});
+
+app.listen(port, () => {
+	console.log(`Listing From Doctors Portal ${port}`);
+});
