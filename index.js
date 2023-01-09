d8b7303a6bd8457a2d46869b8132e126bfef6b6c
rasedprogrammer <rasedprogrammer@gmail.com>
2023-01-09 20:18:54 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -8,6 +9,40 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// MongoDb Connection
+/**
+ * ================================================================================
+ */
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@programmingherocluster.fdfar9q.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+
+// Server configuration
+async function run() {
+	try {
+		const appointmentOptionCollection = client
+			.db("doctorsPortal")
+			.collection("appointmentOptions");
+
+		app.get("/appointmentOptions", async (req, res) => {
+			const query = {};
+			const options = await appointmentOptionCollection.find(query).toArray();
+			res.send(options);
+		});
+	} finally {
+	}
+}
+run().catch(console.log);
+
+/**
+ * ================================================================================
+ */
 app.get("/", async (req, res) => {
 	res.send("Doctors Portal Server Is Running");
 });
